eeabc065048d275fba86a0529038acff6c410ce5
BryantDavidDavis <davis.bryant@gmail.com>
2016-02-14 23:35:54 -0800

    now able to post new products to service

--- models/product.js
+++ models/product.js
@@ -2,13 +2,15 @@
 
 module.exports = function(sequelize, DataTypes) {
 	var Product = sequelize.define("product", {
+		description: DataTypes.STRING,
 		quantity: DataTypes.INTEGER,
 		quantity_counter: DataTypes.STRING,
 		price: DataTypes.DECIMAL,
 		currency: DataTypes.STRING,
 		condition: DataTypes.STRING,
 		latitude: DataTypes.DECIMAL(9,6),
-		longitude: DataTypes.DECIMAL(9,6)
+		longitude: DataTypes.DECIMAL(9,6),
+		vendor_id: DataTypes.INTEGER
 	}, 
 	{		
 		underscored: true,
--- routes/products.js
+++ routes/products.js
@@ -49,4 +49,24 @@ router.get('/:subcategory', function(req, res, next) {
   	});
 });
 
+router.post('/', function(req, res, next) {
+	var datetime = new Date();
+	var form = req.body;
+	db['product'].create({
+		description: form.description,
+		quantity: form.quantity,
+		quantity_counter: form.quantity_counter,
+		price: form.price,
+		currency: form.currency,
+		condition: form.condition,
+		latitude: form.latitude,
+		longitude: form.longitude,
+		subcategory_id: form.subcategory_id,
+		vendor_id: form.vendor_id,
+		created_at: datetime
+	}).then(function(){
+		res.json(form);
+	})
+});
+
 module.exports = router;
